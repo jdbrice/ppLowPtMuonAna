4d3e71c4ffdf535c3232b7762524f67124208ebc
Daniel <jdb12@rice.edu>
2017-06-19 17:44:06 -0500

    prepare for grid submit

---
+++ bin/grid/same.condor.submit.sh
@@ -0,0 +1,10 @@
+######################################################################
+# 				MixedEvent
+######################################################################
+
+InitialDir = /home/jdb12/work/LowPtMuonAna/bin/
+Executable = /home/jdb12/work/LowPtMuonAna/bin/pairAna.app
+
+GetEnv     = True
+
+Queue 82
